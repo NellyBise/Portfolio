5db381d4f465a4aac7346d7b7cbe23a1db5640dc
NellyBise <nellybise@free.fr>
2024-05-28 10:27:15 +0200

    test next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+module.exports = {
+  basePath: '/portfolio/pages/api',
+  assetPrefix: '/portfolio/app/',
+}
 
 export default nextConfig
