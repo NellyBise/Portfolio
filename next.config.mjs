946518bde909c8356d85b2f3376dd2d2bed707cc
NellyBise <nellybise@free.fr>
2024-05-28 10:34:29 +0200

    next config reset

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-module.exports = {
-  basePath: '/portfolio/pages/api',
-  assetPrefix: '/portfolio/app/',
-}
 
 export default nextConfig
