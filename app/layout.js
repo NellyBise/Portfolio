4e1a2842793f9844908e17fe83e4e3fe71687965
NellyBise <nellybise@free.fr>
2025-01-22 18:02:29 +0100

    Add google analytics

--- app/layout.js
+++ app/layout.js
@@ -46,7 +46,7 @@ export default function RootLayout({ children }) {
         className={`${inter.variable} ${league.variable} scroll-smooth`}
       >
         <head>
-          {/* Utilisation de next/script pour Google Tag Manager */}
+          {/* Google Tag Manager Script */}
           <Script
             id="GoogleTagManager"
             strategy="afterInteractive"
@@ -55,24 +55,42 @@ export default function RootLayout({ children }) {
                 (function(w,d,s,l,i){
                   w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
                   var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:''; 
                   j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
                   f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','GTM-PX9CTLPQ');`,
             }}
           />
+          {/* Google Analytics Script */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-644Y34G8MP"
+            strategy="afterInteractive"
+          />
+          <Script
+            id="GoogleAnalytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-644Y34G8MP');`,
+            }}
+          />
         </head>
-        <body className="flex flex-col justify-between min-h-screen">
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-PX9CTLPQ"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
 
+        {/* Noscript for Google Tag Manager */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PX9CTLPQ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
+        <body className="flex flex-col justify-between min-h-screen">
           {children}
         </body>
       </html>
