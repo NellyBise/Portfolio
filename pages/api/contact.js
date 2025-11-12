286da446986179387a7ed9cb09118174d13bb96f
NellyBise <nellybise@free.fr>
2025-11-12 14:53:09 +0100

    Add CORS headers

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -21,6 +21,14 @@ const limiter = rateLimit({
 })
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://nelly-bise.fr')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   limiter(req, res, async () => {
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method Not Allowed' })
